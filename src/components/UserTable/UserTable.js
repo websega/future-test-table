a9340b0f4135b875c179605dedfebeb242d387ab
Sergey Vakhramov <vsa8813@gmail.com>
2021-02-16 16:09:38 +0500

    rename PageSelect to Select

--- src/components/UserTable/UserTable.js
+++ src/components/UserTable/UserTable.js
@@ -1,43 +1,26 @@
 import React from 'react';
-import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-import Pagination from '../Pagination/Pagination';
+import { fetchUsers, sortUsers } from '../../redux/actions';
+
+import Pagination from '../Pagination';
+import Select from '../Select';
 
 import './UserTable.scss';
-import PageSelect from '../PageSelect/PageSelect';
 
 const UserTable = () => {
-  const [users, setUsers] = useState([]);
-  const [sortFlag, setSortFlag] = useState('');
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.users.loading);
+  const users = useSelector((state) => state.users.users);
 
   const [currentPage, setCurrentPage] = useState(1);
   const [usersPerPage, setUsersPerPage] = useState(10);
   const [currentUsers, setCurrentUsers] = useState([]);
 
-  const [loading, setLoading] = useState(false);
-
   useEffect(() => {
-    /*
-    http://www.filltext.com/?rows=1000&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&delay=3&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D
-    */
-    const getUsers = async () => {
-      try {
-        setLoading(true);
-
-        const response = await axios.get(
-          'http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D'
-        );
-
-        setUsers(response.data);
-        setLoading(false);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    getUsers();
-  }, []);
+    dispatch(fetchUsers());
+  }, [dispatch]);
 
   useEffect(() => {
     const indexOfLastPost = currentPage * usersPerPage;
@@ -48,44 +31,9 @@ const UserTable = () => {
 
   const sortHandler = ({ target }) => {
     const cell = target.closest('th');
-    const param = cell.dataset.param;
+    const cellName = cell.dataset.cellName;
 
-    const sortAscending = (a, b) => {
-      if (a[param] > b[param]) {
-        return 1;
-      }
-
-      if (a[param] < b[param]) {
-        return -1;
-      }
-
-      return 0;
-    };
-
-    const sortDesc = (a, b) => {
-      if (a[param] < b[param]) {
-        return 1;
-      }
-
-      if (a[param] > b[param]) {
-        return -1;
-      }
-
-      return 0;
-    };
-
-    console.log(sortFlag);
-
-    if (sortFlag === `${param}-Asc` || sortFlag === '') {
-      const sortedUsers = [...users].sort(sortAscending);
-      setUsers(sortedUsers);
-      setSortFlag(`${param}-Desc`);
-    } else {
-      const sortedUsers = [...users].sort(sortDesc);
-      setUsers(sortedUsers);
-      setSortFlag(`${param}-Asc`);
-    }
-    console.log(sortFlag, users);
+    dispatch(sortUsers(cellName));
   };
 
   const changeSelectHandler = ({ target }) => {
@@ -113,22 +61,22 @@ const UserTable = () => {
       <table className='table table-hover table-striped table-bordered'>
         <thead>
           <tr className='table__row'>
-            {['id', 'firstName', 'lastName', 'email', 'phone'].map((param) => {
-              return (
-                <th
-                  key={param}
-                  data-param={param}
-                  onClick={sortHandler}
-                  className='table__header'
-                >
-                  <div className='table__cell'>
-                    <span>{param}</span>
-                    {sortFlag === `${param}-Desc` ? <span>▲</span> : null}
-                    {sortFlag === `${param}-Asc` ? <span>▼</span> : null}
-                  </div>
-                </th>
-              );
-            })}
+            {['id', 'firstName', 'lastName', 'email', 'phone'].map(
+              (cellName) => {
+                return (
+                  <th
+                    key={cellName}
+                    data-cellName={cellName}
+                    onClick={sortHandler}
+                    className='table__header'
+                  >
+                    <div className='table__cell'>
+                      <span>{cellName}</span>
+                    </div>
+                  </th>
+                );
+              }
+            )}
           </tr>
         </thead>
         <tbody>
@@ -149,7 +97,7 @@ const UserTable = () => {
       </table>
 
       <div className='nav-block'>
-        <PageSelect onChange={changeSelectHandler} />
+        <Select onChange={changeSelectHandler} />
 
         <Pagination
           pageNumbers={Math.ceil(users.length / usersPerPage)}
