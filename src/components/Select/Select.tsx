d6d52ad42580b8237f576c3a040365afa5bf2470
Sergey Vakhramov <vsa8813@gmail.com>
2021-05-27 03:11:36 +0500

    feat(components): extend the select component for reusable use

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,25 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import './Select.scss';
+import classNames from 'classnames';
+
+import TriangleIcon from '../../assets/images/icons/Triangle.svg';
+
+import Button from '../Button';
+
+import classes from './Select.modules.scss';
 
 type SelectPropsType = {
-  onChange: () => void;
+  onClickItem: (userPer: number) => void;
+  items: Array<string>;
 };
 
-const Select = ({ onChange }: SelectPropsType): JSX.Element => (
-  <div className="select">
-    <span className="select__content">Строк на странице:</span>
+const Select = ({ onClickItem, items }: SelectPropsType): JSX.Element => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [activeSuggestion, setActiveSuggestion] = useState<number>(0);
+  const [value, setValue] = useState<string>(items[0]);
 
-    <select
-      className="form-select form-select-sm"
-      aria-label=".form-select-sm example"
-      onChange={onChange}
+  const handleClickItem = (val: string) => {
+    onClickItem(+val);
+    setValue(val);
+    setIsOpen(false);
+  };
+
+  const handleClickKeyDownItem = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    if (
+      (e.code === 'ArrowUp' && activeSuggestion === 0) ||
+      (e.code === 'ArrowDown' && activeSuggestion + 1 === items?.length)
+    ) {
+      return;
+    }
+
+    if (e.code === 'ArrowUp') {
+      setActiveSuggestion(activeSuggestion - 1);
+    }
+
+    if (e.code === 'ArrowDown') {
+      setActiveSuggestion(activeSuggestion + 1);
+    }
+
+    if (e.code === 'Enter' && items) {
+      const userPer = items[activeSuggestion];
+      onClickItem(+userPer);
+      setValue(userPer);
+      setIsOpen(false);
+    }
+  };
+
+  return (
+    <div
+      className={classes.wrapper}
+      role="presentation"
+      onKeyDown={handleClickKeyDownItem}
     >
-      <option value="10">10</option>
-      <option value="25">25</option>
-      <option value="50">50</option>
-    </select>
-  </div>
-);
+      <Button
+        size="s"
+        icon={<TriangleIcon />}
+        rotateIcon={isOpen}
+        onMouseDown={() => setIsOpen(!isOpen)}
+        iconPosition="right"
+      >
+        <div className={classes.value}>{value}</div>
+      </Button>
+
+      {isOpen && (
+        <ul className={classes.list}>
+          {items.map((item, i) => {
+            return (
+              // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+              <li
+                className={classNames(classes.item, {
+                  [classes.active]: activeSuggestion === i,
+                })}
+                onClick={() => handleClickItem(item)}
+                key={item}
+                role="menuitem"
+              >
+                {item}
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
+};
 
 export default Select;
